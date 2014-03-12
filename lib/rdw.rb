885153ccc720ee4609b0d31d7af654042d61bc38
Jankees van Woezik <jankees@base42.nl>
2014-03-12 07:42:38 +0100

    Parse attributes from response

--- lib/rdw.rb
+++ lib/rdw.rb
@@ -3,23 +3,71 @@ require "net/http"
 require "nokogiri"
 
 module RDW
-  def self.car_info_for(license_plate)
-    CarInfo.new(license_plate)
-  end
 
   class CarInfo
-    AVAILABLE_METHODS = %w{Aantalcilinders}
+
     def initialize(license_plate)
-      response = Net::HTTP.get_response(URI("https://api.datamarket.azure.com/Data.ashx/opendata.rdw/VRTG.Open.Data/v1/KENT_VRTG_O_DAT('#{license_plate}')"))
-      @xml = Nokogiri::XML(response.body)
+      @license_plate = license_plate
+      perform_request!
     end
 
-    def method_missing(method, *args, &block)
-      if AVAILABLE_METHODS.include?(method.to_s)
-        @xml.xpath("//d:#{method.to_s}").text
-      else
-        super
-      end
+    def number_of_cylinders
+      parse("Aantalcilinders").to_i
+    end
+
+    def number_of_seats
+      parse("Aantalzitplaatsen").to_i
+    end
+
+    def BPM
+      parse("BPM").to_i
+    end
+
+    def fuel_efficiency_main_road
+      parse("Brandstofverbruikbuitenweg").to_f
+    end
+
+    def fuel_efficiency_city
+      parse("Brandstofverbruikstad").to_f
+    end
+
+    def fuel_efficiency_combined
+      parse("Brandstofverbruikgecombineerd").to_f
+    end
+
+    def cylinder_capacity
+      parse("Cilinderinhoud").to_f
+    end
+
+    def co2_combined
+      parse("CO2uitstootgecombineerd").to_f
+    end
+
+    def color
+      parse("Eerstekleur")
+    end
+
+    def fuel_type
+      parse("Hoofdbrandstof")
+    end
+
+    def brand
+      parse("Merk")
+    end
+
+    def energy_label
+      parse("Zuinigheidslabel")
+    end
+
+  private
+
+    def parse(attribute_name)
+      @xml.xpath("//d:#{attribute_name.to_s}").text
+    end
+
+    def perform_request!
+      response = Net::HTTP.get_response(URI("https://api.datamarket.azure.com/Data.ashx/opendata.rdw/VRTG.Open.Data/v1/KENT_VRTG_O_DAT('#{@license_plate}')"))
+      @xml = Nokogiri::XML(response.body)
     end
   end
 end
---
+++ spec/rdw_car_info_spec.rb
@@ -0,0 +1,71 @@
+require "spec_helper"
+
+describe RDW::CarInfo do
+
+  context "given the license plate of a Ferrari" do
+    let(:license_plate) { "9KJT45" }
+    before { VCR.eject_cassette }
+    before { VCR.insert_cassette("ferrari_9KJT45") }
+    subject { described_class.new(license_plate) }
+
+    it { should be_a RDW::CarInfo }
+
+    its("number_of_cylinders") { should eq 12 }
+    its("number_of_seats") { should eq 2 }
+    its("BPM") { should eq 82168 }
+    its("fuel_efficiency_main_road") { should eq 14.70 }
+    its("fuel_efficiency_city") { should eq 32.70 }
+    its("fuel_efficiency_combined") { should eq 21.30 }
+    its("cylinder_capacity") { should eq 5999.0 }
+    its("co2_combined") { should eq 490.0 }
+    its("color") { should eq "ROOD" }
+    its("fuel_type") { should eq "Benzine" }
+    its("brand") { should eq "FERRARI" }
+    its("energy_label") { should eq "G" }
+  end
+
+  context "given the license plate of a New Beetle" do
+    let(:license_plate) { "87PVF5" }
+    before { VCR.eject_cassette }
+    before { VCR.insert_cassette("new_beetle_9KJT45") }
+    subject { described_class.new(license_plate) }
+
+    it { should be_a RDW::CarInfo }
+
+    its("number_of_cylinders") { should eq 4 }
+    its("number_of_seats") { should eq 4 }
+    its("BPM") { should eq 4563 }
+    its("fuel_efficiency_main_road") { should eq 0.0 }
+    its("fuel_efficiency_city") { should eq 0.0 }
+    its("fuel_efficiency_combined") { should eq 0.0 }
+    its("cylinder_capacity") { should eq 1595.0 }
+    its("co2_combined") { should eq 0.0 }
+    its("color") { should eq "GRIJS" }
+    its("fuel_type") { should eq "Benzine" }
+    its("brand") { should eq "VOLKSWAGEN" }
+    its("energy_label") { should eq "" }
+  end
+
+  context "given the license plate of a T2 bus" do
+    let(:license_plate) { "67YA03" }
+    before { VCR.eject_cassette }
+    before { VCR.insert_cassette("t2_67YA03") }
+    subject { described_class.new(license_plate) }
+
+    it { should be_a RDW::CarInfo }
+
+    its("number_of_cylinders") { should eq 4 }
+    its("number_of_seats") { should eq 0 }
+    its("BPM") { should eq 0 }
+    its("fuel_efficiency_main_road") { should eq 0.0 }
+    its("fuel_efficiency_city") { should eq 0.0 }
+    its("fuel_efficiency_combined") { should eq 0.0 }
+    its("cylinder_capacity") { should eq 0 }
+    its("co2_combined") { should eq 0.0 }
+    its("color") { should eq "BLAUW" }
+    its("fuel_type") { should eq "LPG (Liquified Petrol Gas)" }
+    its("brand") { should eq "VOLKSWAGEN" }
+    its("energy_label") { should eq "" }
+  end
+
+end
